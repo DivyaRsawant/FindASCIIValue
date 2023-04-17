05bdeb49f53f88e291da8720c07d950544393081
Divya Sawant <86359301+DivyaRsawant@users.noreply.github.com>
2023-04-17 20:13:14 +0530

    Add files via upload

---
+++ FindASCIIValue.py
@@ -0,0 +1,9 @@
+# ASCII is American Standard Code for Information Interchange.
+# eg: 1 char to num
+character = input("Enter an Alphabet to find it's ASCII value:")
+print("The Ascii value of" ,character, "is :", ord(character))
+
+
+# eg: 2 num to char
+num = int(input("Enter Number to find it's ASCII value:"))
+print("The Ascii value of", num, "is :", chr(num))
